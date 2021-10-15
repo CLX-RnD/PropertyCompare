b1ee5b335dee5689c1334ab3d34d711719672def
Brandon Deaton <70598871+CLX-BDeaton@users.noreply.github.com>
2021-10-15 22:44:26 +0100

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,6 +6,12 @@ using System.Reflection;
 
 namespace ConsoleApp3
 {
+    public class CompareAttribute : System.Attribute
+    {
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
@@ -13,11 +19,13 @@ namespace ConsoleApp3
             AssetTemplate assetTemplate = new AssetTemplate()
             {
                 ID = "1",
-                Name = "Base Name",
+                Name = null,
+                Comment = "Some comment",
+                UpdatedOn = DateTime.MinValue,
                 Data = new AssetTemplateData()
                 {
                     Description = "Base Description",
-                    AssetTags = new List<AssetTemplateAssetTagData>() { new AssetTemplateAssetTagData() { TagName = "Base Tag Name 1" }, new AssetTemplateAssetTagData() { TagName = "Base Tag Name 2" } }
+                    AssetTags = new List<AssetTemplateAssetTagData>() { new AssetTemplateAssetTagData() { TagName = "Base Tag Name 1" }, new AssetTemplateAssetTagData() { TagName = null } }
                 },
             };
 
@@ -25,9 +33,11 @@ namespace ConsoleApp3
             {
                 ID = "2",
                 Name = "New Name",
-                Data = new AssetTemplateData() { Description = "New Description", 
-                    AssetTags = new List<AssetTemplateAssetTagData>() { new AssetTemplateAssetTagData() { TagName = "New Tag Name 1" }, new AssetTemplateAssetTagData() { TagName = "New Tag Name 2" } } }
-                ,
+                Comment = null,
+                UpdatedOn = DateTime.Now,
+                Data = new AssetTemplateData() { Description = "New Description",
+                    AssetTags = new List<AssetTemplateAssetTagData>() { new AssetTemplateAssetTagData() { TagName = "New Tag Name 1" }, new AssetTemplateAssetTagData() { TagName = "New Tag Name 2" } } 
+                },
             };
 
             Compare(assetTemplate, newAssetTemplate);
@@ -52,72 +62,93 @@ namespace ConsoleApp3
 
 #warning We are using the declared only binding flag to ignore the properties that C# adds onto things like "Lists". However this means inheritance cannot be compared
 
-            var allProperties = baseType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            //var allProperties = baseType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
 
             // Loop through all properties that are public, instanced, and declared only
-            foreach (var property in baseType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            foreach (var property in baseType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
             {
-                // Strings are returning as a char[], so we cannot just use value type
-                if (property.PropertyType.IsValueType || property.PropertyType == typeof(string))
+                if (Attribute.IsDefined(property, typeof(CompareAttribute)))
                 {
-                    var baseValue = property.GetValue(baseObject, null);
-                    var newValue = property.GetValue(newObject, null);
+                    Console.WriteLine($"Comparing {property.Name}");
 
-                    if (!object.Equals(baseValue, newValue))
+                    // Strings are returning as a char[], so we cannot just use value type
+                    if (property.PropertyType.IsValueType || property.PropertyType == typeof(string))
                     {
-                        Console.WriteLine($"{property.Name} changed from {baseValue} to {newValue}");
-                    }
-                }
+                        var baseValue = property.GetValue(baseObject, null);
+                        var newValue = property.GetValue(newObject, null);
 
-                // Check if we are a generic type. This typically means we have a list of data
-                else if (property.PropertyType.IsGenericType)
-                {
-                    // If this is a list type, loop through the list and compare each object in the array
-                    if (property.PropertyType.GetInterfaces().ToList().Where(it => it.Name == "IList").FirstOrDefault() != null)
-                    {
-                        var baseValue = property.GetValue(baseObject, null) as IEnumerable<object>;
-                        var newValue = property.GetValue(newObject, null) as IEnumerable<object>;
+                        if (!object.Equals(baseValue, newValue))
+                        {
+                            if (baseValue == null)
+                                Console.WriteLine($"Set {newValue} to {property.Name}.");
+                            else if (newValue == null)
+                                Console.WriteLine($"Removed {baseValue} from {property.Name}.");
+                            else
+                                Console.WriteLine($"{property.Name} changed from {baseValue} to {newValue}");
 
-                        // Check if the array is the same size or not
-                        if (baseValue.Count() >= newValue.Count())
-                        {
-                            for (int count = 0; count < baseValue.Count(); count++)
-                            {
-                                if (count < newValue.Count())
-                                    Compare(baseValue.ElementAt(count), newValue.ElementAt(count));
-                                else  // There is 1 more value in the old array
-                                    Compare(baseValue.ElementAt(count), null);
-                            }
-                        }
-                        else if (newValue.Count() > baseValue.Count())
-                        {
-                            for (int count = 0; count < newValue.Count(); count++)
-                            {
-                                if (count < baseValue.Count())
-                                    Compare(baseValue.ElementAt(count), newValue.ElementAt(count));
-                                else // There is 1 more value in the new array
-                                    Compare(null, newValue.ElementAt(count));
-                            }
                         }
                     }
+
+                    // Check if we are a generic type. This typically means we have a list of data
+                    else if (property.PropertyType.IsGenericType)
+                    {
+                        // For now, we do not want to process Lists
+                        continue;
+
+                        // If this is a list type, loop through the list and compare each object in the array
+                        if (property.PropertyType.GetInterfaces().ToList().Where(it => it.Name == "IList").FirstOrDefault() != null)
+                        {
+                            var baseValue = property.GetValue(baseObject, null) as IEnumerable<object>;
+                            var newValue = property.GetValue(newObject, null) as IEnumerable<object>;
+
+                            // Check if the array is the same size or not
+                            if (baseValue.Count() >= newValue.Count())
+                            {
+                                for (int count = 0; count < baseValue.Count(); count++)
+                                {
+                                    if (count < newValue.Count())
+                                        Compare(baseValue.ElementAt(count), newValue.ElementAt(count));
+                                    else  // There is 1 more value in the old array
+                                        Compare(baseValue.ElementAt(count), null);
+                                }
+                            }
+                            else if (newValue.Count() > baseValue.Count())
+                            {
+                                for (int count = 0; count < newValue.Count(); count++)
+                                {
+                                    if (count < baseValue.Count())
+                                        Compare(baseValue.ElementAt(count), newValue.ElementAt(count));
+                                    else // There is 1 more value in the new array
+                                        Compare(null, newValue.ElementAt(count));
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.Write($"The property type {property.PropertyType} is not supported.");
+                        }
+                    }
+
+                    // If this is an object, then run compare on that object
+                    else if (property.PropertyType.IsClass)
+                    {
+                        // For now, we do not want to process objects
+                        continue;
+
+                        var baseValue = property.GetValue(baseObject, null);
+                        var newValue = property.GetValue(newObject, null);
+
+                        Compare(baseValue, newValue);
+                    }
+
                     else
                     {
                         Console.Write($"The property type {property.PropertyType} is not supported.");
                     }
                 }
-
-                // If this is an object, then run compare on that object
-                else if (property.PropertyType.IsClass)
-                {
-                    var baseValue = property.GetValue(baseObject, null);
-                    var newValue = property.GetValue(newObject, null);
-
-                    Compare(baseValue, newValue);
-                }
-
                 else
                 {
-                    Console.Write($"The property type {property.PropertyType} is not supported.");
+                    // Do nothing
                 }
             }
         }
@@ -125,13 +156,20 @@ namespace ConsoleApp3
 
     public class DataObject
     {
+        [Compare]
         public string ID { get; set; }
     }
 
 
     public class AssetTemplate : DataObject
     {
+        [Compare]
         public string Name { get; set; }
+
+        [Compare]
+        public string Comment { get; set; }
+
+        public DateTime UpdatedOn { get; set; }
 
         public AssetTemplateData Data { get; set; }
     }
